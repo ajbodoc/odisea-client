e5524c9315fe932ee73037ac5a19e3bd16db56f4
Antonio Jorge Bodoc <ajbodoc@gmail.com>
2022-09-09 18:58:36 -0300

    servidor producción

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import GlobalStyles from './styles';
 import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
+/*
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
+});
+*/
+
+const client = new ApolloClient({
+  uri: 'https://server-catstronauts.herokuapp.com/', 
+  cache: new InMemoryCache()
 });
 
 ReactDOM.render(
